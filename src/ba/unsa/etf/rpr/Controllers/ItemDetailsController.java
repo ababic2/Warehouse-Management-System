941e1075e4770a1eebf2c49ec926bea23ade52cb
ababic <you@example.com>
2020-08-25 11:40:43 +0200

    fixed ItemDetailsController and set Next/Previous button and bidning

--- src/ba/unsa/etf/rpr/Controllers/ItemDetailsController.java
+++ src/ba/unsa/etf/rpr/Controllers/ItemDetailsController.java
@@ -3,64 +3,115 @@ package ba.unsa.etf.rpr.Controllers;
 import ba.unsa.etf.rpr.DAL.DAO.ProductDAO;
 import ba.unsa.etf.rpr.DAL.DTO.Product;
 import ba.unsa.etf.rpr.Interface.ControllerInterface;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.util.converter.NumberStringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ItemDetailsController implements Initializable, ControllerInterface {
 
-    public Label itemIdLable;
-    public TextField itemNameLabel;
+    public Label itemIdLabel;
+    public TextField itemNameField;;
     public TextField itemTypeLabel;
     public TextField itemPriceLabel;
     public Label stockLabell;
+    public Button btnNext;
+    public Button btnPrevious;
+
 
     private SimpleIntegerProperty itemIDLabel;
-    private SimpleStringProperty nameLabel;
-    private SimpleIntegerProperty priceLabel;
-    private SimpleIntegerProperty stockLabel;
-    private SimpleIntegerProperty typeLabel;
+    private SimpleStringProperty name;
+    private SimpleIntegerProperty price;
+    private SimpleIntegerProperty stock;
+    private SimpleStringProperty type;
 
-    private int page = 1;
+    private ObservableList<Product> products = FXCollections.observableArrayList();
+    private ObjectProperty<Product> currentProduct = new SimpleObjectProperty<>();
+    private int page = 0;
+    private boolean disable = true;
     private ProductDAO productDAO = ProductDAO.getInstance();
 
-    public ItemDetailsController(SimpleIntegerProperty itemIDLabel,
-                                 SimpleStringProperty nameLabel, SimpleIntegerProperty priceLabel,
-                                 SimpleIntegerProperty stockLabel, SimpleIntegerProperty typeLabel) {
-        this.itemIDLabel = itemIDLabel;
-        this.nameLabel = nameLabel;
-        this.priceLabel = priceLabel;
-        this.stockLabel = stockLabel;
-        this.typeLabel = typeLabel;
+    //used for SimpleIntegerProperty to SimpleStringProperty
+    private NumberStringConverter converter = new NumberStringConverter();
 
-
+    public ItemDetailsController() {
+        itemIDLabel = new SimpleIntegerProperty(0);
+        name = new SimpleStringProperty("");
+        price = new SimpleIntegerProperty(0);
+        stock = new SimpleIntegerProperty(0);
+        type = new SimpleStringProperty("");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setDisableTo(true);
+        products.addAll(productDAO.getInfoList());
+        bindingFields();
+        btnPrevious.setDisable(true);
+        changeCurrentProduct();
+        initializeFirstProduct();
         setFields();
+        //setDisableTo(true);
+    }
+
+    private void bindingFields() {
+//        itemNameField.textProperty().bindBidirectional(name);
+        itemPriceLabel.textProperty().bindBidirectional(price, converter);
+        itemTypeLabel.textProperty().bindBidirectional(type);
+    }
+
+    private void initializeFirstProduct() {
+        currentProduct.set(products.get(0));
+        itemIDLabel.setValue(currentProduct.getValue().getProductId());
+//        itemNameField.textProperty().bindBidirectional(currentProduct.getValue().nameProperty());
+//        itemPriceLabel.textProperty().bindBidirectional(currentProduct.getValue().getPrice(), converter);
+        itemTypeLabel.textProperty().bindBidirectional(currentProduct.getValue().getCategory().categoryNameProperty());
+        stock.setValue(currentProduct.getValue().getStock());
     }
 
     private void setDisableTo(boolean var) {
-        itemNameLabel.setDisable(var);
+        itemNameField.setDisable(var);
         itemTypeLabel.setDisable(var);
         itemPriceLabel.setDisable(var);
     }
 
     private void setFields() {
-        ObservableList<Product> products = FXCollections.observableArrayList();
-        products.addAll(productDAO.getInfoList());
-        Product currentProduct = products.get(page);
+        itemIDLabel.setValue(currentProduct.getValue().getProductId());
+        currentProduct.addListener((obs, oldValue, newValue) -> {
+            if(oldValue != null) {
+//                itemNameField.textProperty().unbindBidirectional(oldValue.nameProperty());
+                itemPriceLabel.textProperty().unbindBidirectional(oldValue.priceProperty());
+                itemTypeLabel.textProperty().unbindBidirectional(oldValue.getCategory().categoryNameProperty());
+            }
+            if(newValue == null) {
+                currentProduct.set(products.get(0));
+                itemIDLabel.setValue(currentProduct.getValue().getProductId());
+//                itemNameField.textProperty().bindBidirectional(currentProduct.getValue().nameProperty());
+                //itemPriceLabel.textProperty().bindBidirectional(currentProduct.getValue().getPrice(), converter);
+                itemTypeLabel.textProperty().bindBidirectional(currentProduct.getValue().getCategory().categoryNameProperty());
+                stock.setValue(currentProduct.getValue().getStock());
+            }
+            else {
+//                itemNameField.textProperty().bindBidirectional(newValue.nameProperty());
+                itemPriceLabel.textProperty().bindBidirectional(newValue.priceProperty(),converter);
+                itemTypeLabel.textProperty().bindBidirectional(newValue.getCategory().categoryNameProperty());
+            }
+        });
+        stock.setValue(currentProduct.getValue().getStock());
+    }
 
+    private void changeCurrentProduct() {
+        currentProduct.set(products.get(page));
     }
 
     public void onSearchFieldClicked(ActionEvent actionEvent) {
@@ -68,11 +119,35 @@ public class ItemDetailsController implements Initializable, ControllerInterface
     }
 
     public void btnEditClicked(ActionEvent actionEvent) {
-
+        setDisableTo(!disable);
+        disable = !disable;
     }
 
     public void btnDeleteClicked(ActionEvent actionEvent) {
 
+    }
+
+    public void btnNextClicked(ActionEvent actionEvent) {
+        page++;
+        if(page != 0) {
+            btnPrevious.setDisable(false);
+        }
+        if (page == products.size() - 1) {
+            btnNext.setDisable(true);
+        }
+        changeCurrentProduct();
+        setFields();
+    }
+
+    public void btnPrevoiusClicked(ActionEvent actionEvent) {
+            page--;
+            if( page != products.size() - 1 ) {
+                btnNext.setDisable(false);
+            } else if (page == 0) {
+                btnPrevious.setDisable(true);
+            }
+            changeCurrentProduct();
+            setFields();
     }
 
     public int getItemIDLabel() {
@@ -87,51 +162,67 @@ public class ItemDetailsController implements Initializable, ControllerInterface
         this.itemIDLabel.set(itemIDLabel);
     }
 
-    public String getNameLabel() {
-        return nameLabel.get();
+    public String getName() {
+        return name.get();
     }
 
-    public SimpleStringProperty nameLabelProperty() {
-        return nameLabel;
+    public SimpleStringProperty nameProperty() {
+        return name;
     }
 
-    public void setNameLabel(String nameLabel) {
-        this.nameLabel.set(nameLabel);
+    public void setName(String name) {
+        this.name.set(name);
     }
 
-    public int getPriceLabel() {
-        return priceLabel.get();
+    public int getPrice() {
+        return price.get();
     }
 
-    public SimpleIntegerProperty priceLabelProperty() {
-        return priceLabel;
+    public SimpleIntegerProperty priceProperty() {
+        return price;
     }
 
-    public void setPriceLabel(int priceLabel) {
-        this.priceLabel.set(priceLabel);
+    public void setPrice(int price) {
+        this.price.set(price);
     }
 
-    public int getStockLabel() {
-        return stockLabel.get();
+    public int getStock() {
+        return stock.get();
     }
 
-    public SimpleIntegerProperty stockLabelProperty() {
-        return stockLabel;
+    public SimpleIntegerProperty stockProperty() {
+        return stock;
     }
 
-    public void setStockLabel(int stockLabel) {
-        this.stockLabel.set(stockLabel);
+    public void setStock(int stock) {
+        this.stock.set(stock);
     }
 
-    public int getTypeLabel() {
-        return typeLabel.get();
+    public String getType() {
+        return type.get();
     }
 
-    public SimpleIntegerProperty typeLabelProperty() {
-        return typeLabel;
+    public SimpleStringProperty typeProperty() {
+        return type;
     }
 
-    public void setTypeLabel(int typeLabel) {
-        this.typeLabel.set(typeLabel);
+    public void setType(String type) {
+        this.type.set(type);
+    }
+
+    public Product getCurrentProduct() {
+        return currentProduct.get();
+    }
+
+    public ObjectProperty<Product> currentProductProperty() {
+        return currentProduct;
+    }
+
+    public void setCurrentProduct(Product currentProduct) {
+        this.currentProduct.set(currentProduct);
+    }
+
+    public void btnAddClicked(ActionEvent actionEvent) {
+
     }
 }
--- src/ba/unsa/etf/rpr/DAL/DTO/Product.java
+++ src/ba/unsa/etf/rpr/DAL/DTO/Product.java
@@ -1,53 +1,81 @@
 package ba.unsa.etf.rpr.DAL.DTO;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Product {
-    private int productId;
-    private String name;
-    private int price;
-    private int stock;
+
+    private SimpleIntegerProperty productId;
+    private SimpleStringProperty name = new SimpleStringProperty("");
+    private SimpleIntegerProperty price;
+    private SimpleIntegerProperty stock;
     private Category category;
 
     public Product() {
     }
 
     public Product(int productId, String name, int price, int stock, Category category) {
-        this.productId = productId;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
+        this.productId = new SimpleIntegerProperty(productId);
+        this.name = new SimpleStringProperty(name);
+        this.price = new SimpleIntegerProperty(price);
+        this.stock = new SimpleIntegerProperty(stock);
         this.category = category;
     }
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name=" + name +
+                ", price=" + price;
+    }
+
     public int getProductId() {
+        return productId.get();
+    }
+
+    public SimpleIntegerProperty productIdProperty() {
         return productId;
     }
 
     public void setProductId(int productId) {
-        this.productId = productId;
+        this.productId.set(productId);
     }
 
     public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public int getPrice() {
+        return price.get();
+    }
+
+    public SimpleIntegerProperty priceProperty() {
         return price;
     }
 
     public void setPrice(int price) {
-        this.price = price;
+        this.price.set(price);
     }
 
     public int getStock() {
+        return stock.get();
+    }
+
+    public SimpleIntegerProperty stockProperty() {
         return stock;
     }
 
     public void setStock(int stock) {
-        this.stock = stock;
+        this.stock.set(stock);
     }
 
     public Category getCategory() {
